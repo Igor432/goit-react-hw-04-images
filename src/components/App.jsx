e15248e21878cc1cd62538236e9d76643ee813de
Igor432 <igomar0602@gmail.com>
2022-08-04 15:35:53 +0300

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,37 @@
-export const App = () => {
+import '../components/styles.css'
+import axios from 'axios'
+
+const { Component } = require("react")
+
+class App extends Component  {
+
+state = {
+  photoBaza: [],
+  key: ""
+}
+
+
+
+async componentDidMount () {
+
+
+const photos = await axios.get(`https://pixabay.com/api/?q=${this.state.key}&page=1&key=28780636-ee20ed417c8a5aa1eeee48e35&image_type=photo&orientation=horizontal&per_page=12`)
+this.setState({photoBaza: {id: photos.data.hits.id, webformatURL: photos.data.hits.webformatURL, largeImageURL: photos.data.hits.largeImageURL }})
+console.log(this.state.photoBaza)
+
+
+}
+
+
+onSumbit () {
+
+  /*  для інпута в серч барі*/
+}
+
+
+
+render () {
+
   return (
     <div
       style={{
@@ -10,7 +43,11 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      {this.state.photos}
     </div>
-  );
-};
+  )
+
+    }
+  }
+
+  export default App
